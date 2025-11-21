43dcb8712d4c8c0b3eb5e10d88096aa2aeb4185c
RafaelaDias05 <rafaeladias192002@gmail.com>
2025-11-21 17:43:34 -0300

    feat(Middleware-Artisan+Costumer): adiciona verificação de usuário

---
+++ app/Http/Middleware/EnsureUserIsArtisan.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class EnsureUserIsArtisan
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login')
+                ->withErrors(['Você precisa estar logado.']);
+        }
+
+        if (auth()->user()->type !== 'artisan') {
+            return abort(403, 'Acesso negado: somente artesãos podem acessar esta área.');
+        }
+
+        return $next($request);
+    }
+}
