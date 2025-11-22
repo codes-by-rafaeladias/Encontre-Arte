a24f75bb546c083e3bdd8f451d9d49a76e038637
RafaelaDias05 <rafaeladias192002@gmail.com>
2025-11-21 23:36:56 -0300

    style(products): página que lista produtos para o artesão criada

---
+++ resources/views/artisan/products.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@push('styles')
+    @vite(['resources/css/app.css', 'resources/css/artisan/products.css'])
+@endpush
+
+@section('title', 'Meus Produtos')
+
+@section('content')
+<div class="title-action">
+    <h2 class="title">Meus Produtos</h2>
+    <form action="{{ route('produtos.cadastro') }}" method="GET">
+        <button type="submit" class="btn btn-primary btn-medio">
+            Novo Produto
+        </button>
+    </form>
+</div>
+
+    @if($products->isEmpty())
+        <div class="no-items">
+            <p>Você ainda não cadastrou nenhum produto.</p>
+        </div>
+    @else
+
+    <div class="dashboard-grid">
+        @foreach ($products as $product)
+            <x-card_product_artisan :product="$product"/>
+        @endforeach
+    </div>
+
+    @endif
+
+@endsection
--- resources/views/components/card_product_artisan.blade.php
+++ resources/views/components/card_product_artisan.blade.php
@@ -1,28 +1,26 @@
-@vite('resources/css/components/card_product_artisan.css')
-@foreach($produtos as $produto)
-  <div class="card">
-    <img src="{{ asset('storage/' . $produto->imagem) }}" 
-         alt="{{ $produto->nome }}" 
+@vite(['resources/css/app.css','resources/css/components/card_product_artisan.css'])
+@props(['product'])
+<div class="card">
+    <img src="{{ asset('storage/' . $product->image_url) }}" 
+         alt="{{ $product->name }}" 
          class="card-img">
 
-    <h3 class="card-title">{{ $produto->nome }}</h3>
-    <p class="card-price">R$ {{ number_format($produto->preco, 2, ',', '.') }}</p>
+    <h3 class="card-title">{{ $product->name }}</h3>
+    <p class="card-price">R$ {{ number_format($product->price, 2, ',', '.') }}</p>
 
     <div class="card-buttons">
-      <a href="{{ route('produtos.edit', $produto->id) }}" class="btn btn-primario btn-pequeno">
+      <a href="{{ route('produtos.editar', $product->id) }}" class="btn btn-primary btn-pequeno">
         Editar
       </a>
-      <form action="{{ route('produtos.destroy', $produto->id) }}" 
+      <form action="{{ route('produtos.excluir', $product->id) }}" 
             method="POST" 
             onsubmit="return confirm('Tem certeza que deseja excluir este produto?')" 
             style="display:inline">
           @csrf
           @method('DELETE')
-          <button type="submit" class="btn btn-secundario btn-pequeno">
+          <button type="submit" class="btn btn-secondary btn-pequeno">
             Excluir
           </button>
       </form>
     </div>
-  </div>
-@endforeach
-
+</div>
